95d477ae479a33151badd56d304be8ac30eba472
carterdy <selkie_link@hotmail.com>
2017-05-11 17:23:53 -0400

    Added initial work to repo

---
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    /* Most of basis for movement from Code from https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-character-controllers 
+    --since I don't know great ways to do jumping */
+
+    //Maximum speed the character can move
+    public float maxSpeed = 10f;
+    //Strength of jumps
+    public float jumpForce = 10f;
+
+    //Force to "glue" character to ground
+    public float groundingForce = 5f;
+    //Transform used to check where the ground should be
+    public Transform groundCheck;
+    //LayerMask for knowing what is ground
+    public LayerMask whatIsGround;
+    //Slippery material for in air
+    public PhysicsMaterial2D slipperyMat;
+    //Normal material for ground
+    public PhysicsMaterial2D normalMat;
+
+
+    //True if the character is facing right.  Used to flip image
+    bool facingRight = true;
+
+    //To tell when grounded
+    bool grounded = false;
+    //Radius for ground detection
+    float groundRadius = 0.05f;
+
+
+    private Rigidbody2D rb;
+    //Used for null checking GameObjects
+    private GameObject dummy;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        dummy = new GameObject();
+	}
+	
+
+    void FixedUpdate()
+    {
+        //check if grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        if (grounded) {
+            rb.sharedMaterial = normalMat;
+            glueToGround();
+        }
+        else {
+            //Want to make the character slippery in the air to prevent weird stuff like sticking their face to walls
+            rb.sharedMaterial = slipperyMat;
+        }
+
+        //Move character
+        float move = Input.GetAxis("Horizontal");
+        Move(move);
+        //Check to see if we need to flip around
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+        //Check for jumping in here because FixedUpdate can miss inputs
+        if (grounded && Input.GetButton("Jump"))
+        {
+            Jump();
+        }
+	}
+
+    /* Called to flip the character around the y axis */
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+
+    /* Used to control movement */
+    void Move(float movement_in)
+    {
+        rb.velocity = new Vector2 (movement_in * maxSpeed, rb.velocity.y);
+    }
+
+    /* Used to control jumps */
+    void Jump ()
+    {
+        grounded = false;
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    /*  We need to "glue" the character to the ground to prevent them from acting weird on slopes by adding force based off their normal to the slope
+        Found basis of code from https://www.youtube.com/watch?v=xMhgxUFKakQ */
+    void glueToGround()
+    {
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, whatIsGround);
+            //Add force against the normal
+            rb.AddForce(-ray.normal * groundingForce);
+    }
+
+
+    /* Fires the character's projectile weapon.
+    Takes in a GameObject representing the character's current projectile weapon*/
+    void ShootProjectile (GameObject projectile)
+    {
+        //Just have to instantiate the projectile at the right position and let the projectile's script do the rest
+        GameObject active_projectile = Instantiate(projectile) as GameObject;
+        Rigidbody2D proj_rb = active_projectile.GetComponent<Rigidbody2D>();
+    }
+
+}
---
+++ Assets/Scripts/Player/ThrowingRock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingRock : MonoBehaviour {
+
+    //Force that this weapon is thrown with
+    public float throwForce = 10f;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        //Give the rock a kick to get going
+        rb.AddForce(transform.forward * throwForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

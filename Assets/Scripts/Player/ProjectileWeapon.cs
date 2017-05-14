022aaac0cfafedffb40a6f22bd5bd09ac967376d
Dylan <selkie_link@hotmail.com>
2017-05-14 10:35:12 -0400

    Forgot to add new weapon script

---
+++ Assets/Scripts/Player/ProjectileWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ProjectileWeapon : MonoBehaviour {
+
+    //Force that this weapon is thrown with
+    public float throwForce = 10f;
+    public float upForce = 0f;
+
+    //RigidBody2d of the projectile
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

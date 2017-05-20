e0246b9bed1accbe4c131404ce33c5ceb7aa6407
carterdy <selkie_link@hotmail.com>
2017-05-20 17:16:28 -0400

    Fixed sword animation and accidentally started zombie improvement

--- Assets/Scripts/Actors/Monsters/SimpleZombie.cs
+++ Assets/Scripts/Actors/Monsters/SimpleZombie.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SimpleZombie : MeleeAttackingMonster {
 
+    //Low and high range times for random shambling
+    public int shamblePeriodicLowEnd;
+    public int shamblePeriodicHighEnd;
+    //Low and high range duration for shambling
+    public int shambleDurationLowEnd;
+    public int shambleDurationhighEnd;
 
 	// Use this for initialization
 	void Start () {

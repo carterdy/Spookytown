880309d28796812242bdcdea16375be809a546ff
carterdy <selkie_link@hotmail.com>
2017-05-20 16:40:07 -0400

    Added smooth player following camera

---
+++ Assets/Scripts/CentralGameScripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    //Smoothing factor
+    public float smoothing = 5f;
+    //Camera offset
+    public Vector3 offset;
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 targetCamPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPosition, smoothing * Time.deltaTime);
+	}
+}

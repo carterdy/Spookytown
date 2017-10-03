e4368b5f2eefcf9ae7a0a5a462a79cdfd9f5e356
Dylan <selkie_link@hotmail.com>
2017-10-02 20:16:41 -0400

    Added basic state AI scripts

---
+++ Assets/Scripts/Actors/AI/ScriptableObject/Controllers/StateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour {
+
+    public GenericMonster genericMonster;
+    public Transform eyes;
+    public State currentState;
+
+    private bool aiActive;
+
+    private void Awake()
+    {
+        genericMonster = GetComponent<GenericMonster>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!aiActive)
+            return;
+        currentState.updateState(this);
+	}
+
+    private void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+            Gizmos.DrawWireSphere(eyes.position, genericMonster.aggroRange);
+        }
+    }
+}
